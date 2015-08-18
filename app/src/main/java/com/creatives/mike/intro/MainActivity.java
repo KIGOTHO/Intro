9a9f5ac144151aa593cbeed712b889db99414290
michael <mkigotho2@gmail.com>
2015-08-19 00:35:39 +0300

    importing more required

--- app/src/main/java/com/creatives/mike/intro/MainActivity.java
+++ app/src/main/java/com/creatives/mike/intro/MainActivity.java
@@ -1,5 +1,17 @@
 package com.creatives.mike.intro;
 
+import android.app.Fragment;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
 
 
 public class MyIntro extends AppIntro {
