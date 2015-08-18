00a6467008bb50c14fc7f11c21b8c3079ec50431
michael <mkigotho2@gmail.com>
2015-08-19 00:34:04 +0300

    create a hide and skip buttonn

--- app/src/main/java/com/creatives/mike/intro/MainActivity.java
+++ app/src/main/java/com/creatives/mike/intro/MainActivity.java
@@ -19,7 +19,12 @@ public class MyIntro extends AppIntro {
         setBarColor(Color.parseColor("#3F51B5"));
         setSeparatorColor(Color.parseColor("#2196F3"));
 
-        
+        // Hide Skip/Done button
+        showSkipButton(false);
+        showDoneButton(false);
+
+        // Turn vibration on and set intensity
+        // NOTE: you will probably need to ask VIBRATE permesssion in Manifest
         setVibrate(true);
         setVibrateIntensity(30);
     }
